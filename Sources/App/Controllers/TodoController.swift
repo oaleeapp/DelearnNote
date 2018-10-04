bea380c1321cbc5cb870484b28a0fc448637ea01
Victor Lee <specialvict@gmail.com>
2018-10-05 01:27:09 +0800

    Add webhook to slack

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -4,12 +4,33 @@ import Vapor
 final class TodoController {
     /// Returns a list of all `Todo`s.
     func index(_ req: Request) throws -> Future<[Todo]> {
+        Todo.query(on: req).all().map(to: [Todo].self) { todoList in
+            let todoListString = todoList.reduce("Todo list(\(todoList.count)): \n"){$0 + "  \($1.id!). \($1.title) \n"}
+            print("todoListString \(todoListString)")
+            let res = try req.client().post("https://hooks.slack.com/services/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"){ post in
+                post.http.contentType = .json
+                try post.content.encode("""
+                    {"text": "\(todoListString)"}
+                    """)
+            }
+            print(res)
+            return todoList
+        }
+
         return Todo.query(on: req).all()
     }
 
     /// Saves a decoded `Todo` to the database.
     func create(_ req: Request) throws -> Future<Todo> {
         return try req.content.decode(Todo.self).flatMap { todo in
+            let res = try req.client().post("https://hooks.slack.com/services/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"){ post in
+                post.http.contentType = .json
+                try post.content.encode("""
+                {"text": "Add `\(todo.title)` to the list"}
+                """)
+            }
+            print("todo: \(todo.title)")
+            debugPrint(res)
             return todo.save(on: req)
         }
     }
